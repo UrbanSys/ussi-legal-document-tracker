1e369b7416b68c1419f5e009c4310c20ed1db57a
gsuofc <113817803+gsuofc@users.noreply.github.com>
2025-10-09 14:29:52 -0600

    file headers

---
+++ actions.py
@@ -0,0 +1,7 @@
+"""
+actions.py
+Gavin Schultz 2025
+Contains functions to call when actions are needed to be done (i.e. clicking a button on an UI). This is 
+where I define the actual behaivours for what the UI does. This should be done in a way so that the UI can be 
+interchanged as needed, and all that needs to be done is have the UI point to these functions to do everything.
+"""
