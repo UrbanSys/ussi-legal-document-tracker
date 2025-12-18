ba6a8c28c8afb9da0d0d204c3ece5a91ac68675a
gsuofc <113817803+gsuofc@users.noreply.github.com>
2025-12-18 11:24:50 -0700

    Add dropdowns to excel table

--- utils.py
+++ utils.py
@@ -217,7 +217,7 @@ def load_config(config_file):
 
     return (full_discharges,partial_discharges,consents)
 
-def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = []):
+def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = [],proj_num="0000.0000.00"):
     print("exporting")
 
     #print(encumbrances)
@@ -246,6 +246,8 @@ def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = [])
         }
     )
 
+    base_format = workbook.add_format({"border": 1})
+
     worksheet.set_column("A:A", 25)
     worksheet.set_column("B:B", 25)
     worksheet.set_column("C:C", 40)
@@ -254,8 +256,14 @@ def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = [])
     worksheet.set_column("F:F", 45)
     worksheet.set_column("G:G", 33)
 
+    action_options = ["NO ACTION REQUIRED","CONSENT","PARTIAL DISCHARGE","FULL_DISCHARGE"]
+    options = ["---","Prepared","Complete","No Action Required","Client for Execution","City for Execution","Third party for Execution"]
+
+    firstrow = 0
+    lastrow = 0
+
     row = 0
-    write_header_row(worksheet, row,7,title_format,"0000.0000.00 - PROJECT - DOCUMENT TRACKING")
+    write_header_row(worksheet, row,7,title_format,"%s - PROJECT - DOCUMENT TRACKING"%proj_num)
     row += 1
     for plan_name, title in encumbrances.items():
         write_header_row(worksheet, row,7,section_format,"EXISTING ENCUMBRANCES ON TITLE - %s"%plan_name)
@@ -264,13 +272,33 @@ def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = [])
         worksheet.write(row,0,"Item #",title_format)
         for index, key in enumerate(table_keys):
             worksheet.write(row,index+1,key,title_format)
+        firstrow = row+1
         row +=1
         for item_no, line in enumerate(title):
-            worksheet.write(row,0,item_no)
+            worksheet.write(row,0,item_no+1,base_format)
             for index, key in enumerate(table_keys):
                 if key in line:
-                    worksheet.write(row,index+1,line[key])
+                    if key == "Action":
+                        line[key] = line[key].upper()
+                    worksheet.write(row,index+1,line[key],base_format)
             row+=1
+        lastrow = row-1
+        
+        worksheet.data_validation(
+            "G%i:G%i"%(firstrow+1,lastrow+1),
+            {
+                "validate": "list",
+                "source": options,
+            },
+        )
+
+        worksheet.data_validation(
+            "E%i:E%i"%(firstrow+1,lastrow+1),
+            {
+                "validate": "list",
+                "source": action_options,
+            },
+        )
 
     for plan_name, plan in plans.items():
         write_header_row(worksheet, row,7,section_format,"PLAN - %s"%plan_name)
@@ -279,13 +307,23 @@ def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = [])
         worksheet.write(row,0,"Item #",title_format)
         for index, key in enumerate(table_keys):
             worksheet.write(row,index+1,key,title_format)
+        firstrow = row+1
         row +=1
         for item_no, line in enumerate(plan):
-            worksheet.write(row,0,item_no)
+            worksheet.write(row,0,item_no+1,base_format)
             for index, key in enumerate(table_keys):
                 if key in line:
-                    worksheet.write(row,index+1,line[key])
+                    worksheet.write(row,index+1,line[key],base_format)
             row+=1
+        lastrow = row-1
+        
+        worksheet.data_validation(
+            "G%i:G%i"%(firstrow+1,lastrow+1),
+            {
+                "validate": "list",
+                "source": options,
+            },
+        )
 
 
     write_header_row(worksheet, row,7,section_format,"NEW AGREEMENTS CONCURRENT WITH REGISTRATION")
@@ -294,13 +332,57 @@ def export_as_excel(filename,encumbrances = [], plans = {}, new_agreements = [])
     worksheet.write(row,0,"Item #",title_format)
     for index, key in enumerate(table_keys):
         worksheet.write(row,index+1,key,title_format)
+    firstrow = row+1
     row +=1
     for item_no, line in enumerate(new_agreements):
-        worksheet.write(row,0,item_no)
+        worksheet.write(row,0,item_no+1,base_format)
         for index, key in enumerate(table_keys):
             if key in line:
-                worksheet.write(row,index+1,line[key])
+                worksheet.write(row,index+1,line[key],base_format)
         row+=1
+
+    lastrow = row-1
+        
+    worksheet.data_validation(
+        "G%i:G%i"%(firstrow+1,lastrow+1),
+        {
+            "validate": "list",
+            "source": options,
+        },
+    )
+
+
+    prepared_fmt = workbook.add_format({"bg_color": "#C6FFAC"})
+    complete_fmt  = workbook.add_format({"bg_color": "#8F8F8F"})
+    client_fmt = workbook.add_format({"bg_color": "#FDF8CD"})
+
+    # Apply conditional formatting (rows 1–100, columns A–G)
+    worksheet.conditional_format(
+        "A1:G100",
+        {
+            "type": "formula",
+            "criteria": '=$G1="Prepared"',
+            "format": prepared_fmt,
+        },
+    )
+
+    worksheet.conditional_format(
+        "A1:G100",
+        {
+            "type": "formula",
+            "criteria": '=OR($G1="Complete",$G1="No Action Required")',
+            "format": complete_fmt,
+        },
+    )
+
+    worksheet.conditional_format(
+        "A1:G100",
+        {
+            "type": "formula",
+            "criteria": '=$G1="Client for Execution"',
+            "format": client_fmt,
+        },
+    )
 
     workbook.close() 
 
